ce9cf999eb99de6a811d8793f6831dd38814336c
SleythergCP <sleyther.calsin@cloudcomputing.com.pe>
2022-11-17 18:23:13 -0500

    Route: Componentes de Navegación

---
+++ src/components/Menu.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+function Menu() {
+ return (
+  <>
+   <p>Menu</p>
+  </>
+ );
+}
+
+export { Menu };
---
+++ src/components/ProfilePage.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function ProfilePage() {
+ return (
+  <>
+   <h1>ProfilePage</h1>
+  </>
+ );
+}
+
+export { ProfilePage };
